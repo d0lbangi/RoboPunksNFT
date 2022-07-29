98cca52e006489dc6874062ff579bab8dd95355a
Dev_Hygge <104881579+d0lbangi@users.noreply.github.com>
2022-07-29 19:34:09 +0900

    FEAT: Production

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-ethers");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();  
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.REACT_APP_RINKEBY_RPC_URL,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.REACT_APP_ETHERSCAN_API_KEY,
+  },
+};
